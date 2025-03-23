05391ac2f25a9c9aec515974d4de9009d11b0b0a
JannaDev-py <orlandojannael@gmail.com>
2025-03-23 07:43:01 -0600

    refactorization using POO, moveBall, for the two screen bot and player interaction

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const $btnPlay = document.querySelector('.btn-play');
-
 const currentPage = (location.href.split("?")[1] === "2") ? "2" : "1";
-//replace the class of the container with the current page
-document.querySelector('.container').classList.replace(`container`, `container-${currentPage}`);
+localStorage.setItem(`innerWidth-${currentPage}`, innerWidth);
+localStorage.setItem(`innerHeight-${currentPage}`, innerHeight);
+let isPlaying = false;
+
 
 class bot {
-    constructor(difficult){
+    constructor(element, difficult){
+        this.element = element;
         if(difficult === "Easy"){
             this.speed = 3;
         }else if(difficult === "Medium"){
@@ -15,6 +16,11 @@ class bot {
         }
     }
     move(){
+        const root = document.querySelector(":root");
+        const top = Number(localStorage.getItem("ballPositionTop"));
+        const left = Number(localStorage.getItem("ballPositionLeft"));
+        root.style.setProperty("--top-bot", top + "px");
+        root.style.setProperty("--top-user", left + "px");
     }
 }
 
@@ -29,16 +35,16 @@ class player {
             return false; //avoid go out of the screen
         }
         this.position = this.position - this.speed;
-        element.style.setProperty("--top-user", this.position + "px");
-        localStorage.setItem("player", this.position)
+        this.element.style.setProperty("--top-user", this.position + "px");
+        localStorage.setItem("playerPosition", this.position)
     }
     moveDown(){
         if(this.position >= innerHeight - 100){
             return false; //avoid go out of the screen
         }
         this.position = this.position + this.speed;
-        element.style.setProperty("--top-user", this.position + "px");
-        localStorage.setItem("player", this.position)
+        this.element.style.setProperty("--top-user", this.position + "px");
+        localStorage.setItem("playerPosition", this.position)
     }
 }
 
@@ -66,5 +72,77 @@ class ball {
         this.positionY = this.positionY + this.speedY;
         this.element.style.setProperty("top", this.positionY + "px");
         this.element.style.setProperty("left", this.positionX + "px");
+        localStorage.setItem("ballPositionTop", this.positionY); 
+        localStorage.setItem("ballPositionLeft", this.positionX);
+    }
+    moveOflocalStorageSecondScreen(){
+        const top = Number(localStorage.getItem("ballPositionTop"));
+        const left = Number(localStorage.getItem("ballPositionLeft")) - Number(localStorage.getItem("innerWidth-1"));
+        this.element.style.setProperty("top", top + "px");
+        this.element.style.setProperty("left", left + "px");
     }
 }
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    const $btnPlay = document.querySelector('.btn-play');
+    let botOnSecondPage = new bot("hard");
+    let playerFirstScreen = undefined;
+
+    //height and width of the two pages
+    let totalWidth = Number(localStorage.getItem(`innerWidth-1`)) + Number(localStorage.getItem(`innerWidth-2`));
+    let minHeight = Math.min(Number(localStorage.getItem(`innerHeight-1`)), Number(localStorage.getItem(`innerHeight-2`)));
+    let ballToPlay = new ball(document.querySelector(".ball"), innerWidth / 2, innerHeight / 2, 5, 5, totalWidth, minHeight);
+
+    //validate if we are on the second page
+    if(currentPage === "2"){
+        document.querySelector('.ask-players').style.display = 'none';
+        document.querySelector('.container-1').classList.replace(`container-1`, `container-2`);
+        if(localStorage.getItem("botDifficult") !== null){
+            botOnSecondPage = new bot(localStorage.getItem("botDifficult"));
+        }
+        const updateBot = ()=>{
+            requestAnimationFrame(updateBot);
+            botOnSecondPage.move();
+            ballToPlay.moveOflocalStorageSecondScreen();
+        }
+        updateBot();
+    }else{
+        localStorage.setItem("playerPosition", 100);
+        playerFirstScreen = new player(document.querySelector(".user"), Number(localStorage.getItem("playerPosition")), 25);
+        const updateBall = ()=>{
+            requestAnimationFrame(updateBall);
+            ballToPlay.move();
+        }
+        updateBall();
+    }
+    
+    $btnPlay.addEventListener('click', () => {
+        document.querySelector('.ask-players').style.display = 'none';
+    
+        //ask for the difficulty
+        const $selectDificult = document.querySelector('.select-dificult');
+        const $btns = $selectDificult.querySelectorAll('button');
+    
+        //show the dificult selector
+        $selectDificult.style.display = 'flex';
+        $btns.forEach(($btn) => {
+            $btn.addEventListener('click', () => {
+                $selectDificult.style.display = 'none';
+                isPlaying = true;
+                document.querySelector('.ask-players').style.display = 'none';
+                window.open(location.href.split("?")[0] + "?2", "_blank");
+                localStorage.setItem("botDifficult", $btn.innerText);
+            });
+        });
+    });
+    
+    document.addEventListener("keydown", (e)=>{
+        const keyUp = "w";
+        const keyDown = "s"
+        if(e.key === keyUp){
+            playerFirstScreen.moveUp();
+        }else if(e.key === keyDown){
+            playerFirstScreen.moveDown();
+        }
+    })
+})
